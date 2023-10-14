2a725b3a57d005642a002e7214f936778cb7b97f
Joaquin Damian Seita <joaquindamianseita@gmail.com>
2023-10-14 15:58:09 -0300

    update log

--- config/database.go
+++ config/database.go
@@ -13,7 +13,7 @@ func ConnectToDB() *gorm.DB {
 	var err error
 	dsn := os.Getenv("DB_DSN")
 
-	log.Panicln("DSN LOGGED: ", os.Getenv("DB_DSN"))
+	log.Println("DSN LOGGED: ", os.Getenv("DB_DSN"))
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
