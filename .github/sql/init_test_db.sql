1d6c22379403ed1454278a3c31b1d602f3296da7
Joaquin Damian Seita <joaquindamianseita@gmail.com>
2023-10-14 14:38:10 -0300

    update workflow

---
+++ .github/sql/init_test_db.sql
@@ -0,0 +1,13 @@
+-- Database: gogin-api-base-test
+
+-- DROP DATABASE IF EXISTS "gogin-api-base-test";
+
+CREATE DATABASE "gogin-api-base-test"
+    WITH
+    OWNER = root
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Spanish_Argentina.1252'
+    LC_CTYPE = 'Spanish_Argentina.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
